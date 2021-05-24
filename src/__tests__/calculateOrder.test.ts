7c23f86e7cfe7f02cde4037cf33f4f53d552bf2d
wiktor <wiktorkujawa1993@gmail.com>
2021-05-25 01:12:29 +0200

    Created first materials module and first src functions

---
+++ src/__tests__/calculateOrder.test.ts
@@ -0,0 +1,4 @@
+import { calculateOrder } from '../index';
+test('My Greeter', () => {
+  expect(calculateOrder(20,10)).toBe(3.4);
+});
---
+++ src/materials.ts
@@ -0,0 +1,68 @@
+const materials = {
+  metalFrames: {
+    uTracks: 0,
+    cStoods: 0,
+  },
+  plasterBoards: {
+    Standard: {
+      Standard4x8: {
+        width: 4,
+        height: 8,
+        thickness: 0.5,
+        quantity:0
+      },
+      Standard4_5x8: {
+        width: 4.5,
+        height: 8,
+        thickness: 0.5,
+        quantity:0
+      },
+      Standard4x10: {
+        width: 4,
+        height: 10,
+        thickness: 0.5,
+        quantity:0
+      },
+      Standard4_5x10: {
+        width: 4.5,
+        height: 10,
+        thickness: 0.5,
+        quantity:0
+      },
+    },
+    Insulated: {
+      Insulated4x8: {
+        width: 4,
+        height: 8,
+        thickness: 0.5,
+        quantity:0
+      },
+      Insulated4_5x8: {
+        width: 4.5,
+        height: 8,
+        thickness: 0.5,
+        quantity:0
+      },
+      Insulated4x10: {
+        width: 4,
+        height: 10,
+        thickness: 0.5,
+        quantity:0
+      },
+      Insulated4_5x10: {
+        width: 4.5,
+        height: 10,
+        thickness: 0.5,
+        quantity:0
+      },
+    },
+  },
+  fixings: {
+    waferHeadScrew: 0,
+    nailablePlugs: 0,
+    highTreadScrews: 0,
+    selfTapingSrews: 0,
+  },
+};
+
+export default materials;
